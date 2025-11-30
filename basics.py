b03bad235077bae3546f9ea3aa45ca85f59d8143
srinivasreddy-12 <srinivasreddy76010@gmail.com>
2025-11-30 22:44:52 +0530

    basics regarding variables

--- basics.py
+++ basics.py
@@ -2,4 +2,14 @@
 a = 10
 b = 30
 c = a + b
-print("Sum of two numbers are ",c);
+print("Sum of two numbers are ",c); #we can keep semicolon because it is exception for python
+
+d = 79 ; e = 31 #we can assign two variables in one line by using semicolon
+print("difference between d and e is : ",d - e)
+
+A,g = 3,4 #Another approch to assign variables
+print(A+g)
+
+#printing memory address
+print(id(a)) #python is a case sensitive language
+print(id(A))
